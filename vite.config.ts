2b164ce9122552c59b5610fa53527a3b3bfe3764
Ui_WIll <williamemanoel777@gmail.com>
2025-02-24 09:05:58 -0300

    Automatico

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HOPE/', // Adicione o nome do seu repositório aqui
+  base: "/HOPE", // Adicione o nome do seu repositório aqui
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
